ad622af13b1213612d0dbccd2a1de3ac7b0c64e5
gspinaci <gianmarco.spinaci@gmail.com>
2023-07-05 16:08:24 +0200

    update routes to match manifest ID
    Signed-off-by: gspinaci <gianmarco.spinaci@gmail.com>

--- routes/manifest_routes.go
+++ routes/manifest_routes.go
@@ -7,8 +7,7 @@ import (
 )
 
 func ManifestRoutes(router *gin.Engine) {
-	router.GET("/", controllers.Ping())
-	router.GET("/manifest/:manifestId", controllers.GenerateManifestByID())
+	router.GET("/:manifestId", controllers.GenerateManifestByID())
 	router.GET("/manifest-data/:manifestId", controllers.GenerateManifestByID())
 	router.POST("/manifest", controllers.CreateManifestMetadata())
 }
